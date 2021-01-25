3aa5e6403bc79e88efdc26ac0880c383348f7a34
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2021-01-25 11:52:30 +0100

    mongoose registeruser

--- 1-mongoose-hash/models/db.js
+++ 1-mongoose-hash/models/db.js
@@ -1,3 +1,79 @@
 const mongoose = require('mongoose');
-
+// get connectionstring from mongodbatlas
 const connectionString = 'mongodb+srv://fbw2lei:!234qweR@cluster0.rmrmn.mongodb.net/fbw2lei?retryWrites=true&w=majority'
+
+// create the schema for user
+const Schema = mongoose.Schema;
+
+const usersSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    userName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+// create the mongoose model to be connected the userSchema with [users] collection
+
+const Users = mongoose.model('users', usersSchema);
+
+
+function connect() {
+    return new Promise((resolve, reject) => {
+        if (mongoose.connection.readyState === 1) {
+            resolve();
+        } else {
+            mongoose.connect(connectionString, {
+                useUnifiedTopology: true,
+                useCreateIndex: true,
+                useNewUrlParser: true
+            }).then(() => {
+                resolve();
+            }).catch(error => {
+                reject(error)
+            })
+        }
+    })
+}
+
+function registerUser(fname, lname, username, email, password) {
+    return new Promise((resolve, reject) => {
+        connect().then(() => {
+            const newUser = new Users({
+                firstName: fname,
+                lastName: lname,
+                userName: username,
+                email,        //email: email
+                password
+            });
+            newUser.save().then(result => {
+                resolve(result);
+            }).catch(error => {
+                reject(error)
+            })
+        }).catch(error => {
+            reject(error)
+        })
+    })
+}
+
+module.exports = {
+    registerUser
+}
